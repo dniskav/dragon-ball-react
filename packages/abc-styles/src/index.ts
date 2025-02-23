73cb267d2b53165662e208a15547befb2e4db568
Dniskav <dniskav@gmail.com>
2025-02-24 00:43:53 +0100

    fix card and favicon

--- packages/abc-styles/src/index.ts
+++ packages/abc-styles/src/index.ts
@@ -1,1 +1,3 @@
 export { default as AbcButton } from "./Button/AbcButton";
+export { default as AbcCard } from "./Card/AbcCard";
+export { default as AbcFavIcon } from "./FavoritesIcon/AbcFavIcon";
--- packages/dragon-ball-app/src/App.tsx
+++ packages/dragon-ball-app/src/App.tsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { AbcButton } from 'abc-styles'
 import './App.css'
+import { AbcButton, AbcCard } from 'abc-styles';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [favorite, setFavorite] = useState(false)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
         <AbcButton onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </AbcButton>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        <AbcButton onClick={() => setFavorite(f => !f)}>
+          fav {favorite}
+        </AbcButton>
+
+        <AbcCard label="titulo 1" isFavorite={favorite} >
+          <img src="https://picsum.photos/188/245" alt="Card 1" />
+        </AbcCard>
     </>
   )
 }
