142c8626dee7dc73e686e969436d0ad98544af9b
Dniskav <dniskav@gmail.com>
2025-02-24 16:18:57 +0100

    refactor

--- packages/abc-styles/src/index.ts
+++ packages/abc-styles/src/index.ts
@@ -1,3 +1,5 @@
+import './styles/common.css';
+
 export { default as AbcButton } from "./Button/AbcButton";
 export { default as AbcCard } from "./Card/AbcCard";
 export { default as AbcFavIcon } from "./FavoritesIcon/AbcFavIcon";
