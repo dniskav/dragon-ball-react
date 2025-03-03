fe070bf3d1a160a89985d169853f4802f9dcd1c3
Dniskav <dniskav@gmail.com>
2025-03-03 13:38:26 +0100

    add setup for github pages

---
+++ packages/dragon-ball-app/docs/images/index.ts
@@ -0,0 +1,7 @@
+import heartIconFilled from "./HeartIconFilled.png"
+import heartIconFilledBig from "./HeartIconFilledBig.png"
+import HeartIconBig from "./HeartIconBig.svg"
+import HeartIconRed from "./HeartIconRed.svg"
+import HeartIconStroke from "./HeartIconStroke.svg"
+
+export { heartIconFilled, heartIconFilledBig, HeartIconBig, HeartIconRed, HeartIconStroke }
