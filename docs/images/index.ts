d3f0132d3f7210c766360735812855e9b0ae638b
Dniskav <dniskav@gmail.com>
2025-03-07 11:57:30 +0100

    gh-pages

--- docs/images/index.ts
+++
@@ -1,7 +0,0 @@
-import heartIconFilled from "./HeartIconFilled.png"
-import heartIconFilledBig from "./HeartIconFilledBig.png"
-import HeartIconBig from "./HeartIconBig.svg"
-import HeartIconRed from "./HeartIconRed.svg"
-import HeartIconStroke from "./HeartIconStroke.svg"
-
-export { heartIconFilled, heartIconFilledBig, HeartIconBig, HeartIconRed, HeartIconStroke }
